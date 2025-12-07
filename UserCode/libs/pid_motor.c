34129674c3332a717454cb65024d8b4f042aaaf0
syhanjin <syhanjin666@gmail.com>
2025-12-07 21:20:34 +0800

    deps: add C++ compatibility to PID motor module

--- UserCode/libs/pid_motor.c
+++ UserCode/libs/pid_motor.c
@@ -24,6 +24,11 @@
 #include "pid_motor.h"
 #include <string.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 void MotorPID_Calculate(MotorPID_t* hpid)
 {
     hpid->cur_error = hpid->ref - hpid->fdb;
@@ -49,3 +54,7 @@ void MotorPID_Init(MotorPID_t* hpid, const MotorPID_Config_t pid_config)
     hpid->Kd             = pid_config.Kd;
     hpid->abs_output_max = pid_config.abs_output_max;
 }
+
+#ifdef __cplusplus
+}
+#endif
